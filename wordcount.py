88f624cf24bfcd9b191c090c17c86fdfb4139bb2
Zhengji Li <z648li@student.cs.uwaterloo.ca>
2020-11-15 15:07:26 -0500

    Add wordcount

---
+++ wordcount.py
@@ -0,0 +1,28 @@
+import pyspark
+from pyspark.sql import SparkSession
+
+from nltk.corpus import stopwords
+import re
+
+stopwords_en = stopwords.words('english')
+
+N = 1000 # top N words
+
+def wordcount(row):
+  text = row.__getitem__('text')
+  words = map(lambda t: re.sub('^([^A-Za-z0-9])+|([^A-Za-z0-9])+$', '', t.lower()), text.split(' '))
+  for w in words:
+    if len(w) > 1 and w not in stopwords_en:
+      yield (w, 1)
+
+if __name__ == '__main__':
+  spark = SparkSession.builder.getOrCreate()
+
+  tweets = spark.read.option('multiline', True).option('escape', '"').csv('covid19_tweets.csv', header=True)
+  tweets = tweets.sample(False, 0.5) # sample 10% of total rows
+
+  x = tweets.select('text').dropna()
+  result = x.rdd.flatMap(wordcount).reduceByKey(lambda l, r: l+r).takeOrdered(N, key=lambda r: -r[1])
+  for r in result:
+    print((r[1], r[0]))
+  
